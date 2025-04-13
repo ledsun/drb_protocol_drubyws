20f1d28c0e9d04370e653d5599c71a42e143b15d
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-04-13 18:01:36 +0900

    Add browser example

---
+++ examples/browser/hello_s.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "drb_web_socket"
+
+# This is a sample class.
+class Foo
+  def greeting = puts "Hello, world!"
+end
+
+DRb.start_service "drubyws://localhost:12345", Foo.new
+sleep
