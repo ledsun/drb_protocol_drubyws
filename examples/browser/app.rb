44c36eafc1a3ea1fc9dff3da8fef3887678c3259
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-04-17 09:05:38 +0900

    Fix style

--- examples/browser/app.rb
+++ examples/browser/app.rb
@@ -1,25 +1,29 @@
-$LOAD_PATH.unshift File.expand_path('lib', __dir__)
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("lib", __dir__)
 require "drb_web_socket"
 
-class DRb::DRbConn
-  class HashStore
-    def initialize
-      @pool = {}
+module DRb
+  class DRbConn
+    class HashStore
+      def initialize
+        @pool = {}
+      end
+
+      def take(key)
+        key if @pool[key]
+      end
+
+      def store(key)
+        @pool[key] = true
+      end
     end
 
-    def take(key)
-      key if @pool[key]
-    end
-
-    def store(key)
-      @pool[key] = true
+    def self.make_pool
+      HashStore.new
     end
   end
-
-  def self.make_pool
-    HashStore.new
-  end
- end
+end
 
 foo = DRbObject.new_with_uri("drubyws://localhost:12345")
 foo.greeting
